3841d7b4a6c1ddc65956bd0a993977ae0ed093f2
fuqiang <fuqiang@uniontech.com>
2020-04-24 14:31:53 +0800

    Type:chore
    Description:update translations
    
    RootCause:Null
    
    Solution:update translations

--- src/translations/policy/policy_zh_CN.ts
+++ src/translations/policy/policy_zh_CN.ts
@@ -3,13 +3,13 @@
 		<name>policy</name>
 		<message>
 			<location filename="com.deepin.pkexec.logViewerAuth!message" line="0"/>
-			<source>Authentication is required to view kernel logs</source>
-			<translation>查看内核日志需要认证</translation>
+			<source>Authentication is required to view the log</source>
+			<translation>查看此日志需要认证</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.pkexec.logViewerAuth!description" line="0"/>
-			<source>Authentication is required to view boot logs</source>
-			<translation>查看启动日志需要认证</translation>
+			<source>authentication</source>
+			<translation>认证</translation>
 		</message>
 	</context>
 </TS>
