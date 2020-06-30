b09106a037b76347ffeac3466c02e220c2ccf7ef
zyc <zyc@zyc-PC>
2020-06-30 14:46:13 +0800

    fix: 禁用高分屏适配属性
    Description: 禁用高分屏适配属性,询问项目经理后禁止使用。
    
    Log: 禁用高分屏适配属性
    
    link:

--- application/main.cpp
+++ application/main.cpp
@@ -35,7 +35,7 @@ DCORE_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
-    DApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    //DApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     DApplication::loadDXcbPlugin();
     DApplication a(argc, argv);
 
