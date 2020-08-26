872f7b74facadb5a06e7a937008aeda6df0d74ac
zyc <zyc@zyc-PC>
2020-08-26 15:52:47 +0800

    fix: 1.修复了dbus接口session错误的问题      2.修复了klu上搜索框不见的问题
    1.修改session为systemsession
    2.去除搜索框父对象
    
    Log: 1.修复了dbus接口session错误的问题
         2.修复了klu上搜索框不见的问题
    
    link:

--- application/dbusmanager.cpp
+++ application/dbusmanager.cpp
@@ -8,7 +8,7 @@ DBusManager::DBusManager(QObject *parent) : QObject(parent)
 
 QString DBusManager::getSystemInfo()
 {
-    QString systemName =  QDBusInterface("com.deepin.system.SystemInfo", "/com/deepin/system/SystemInfo", "com.deepin.system.SystemInfo")
+    QString systemName =  QDBusInterface("com.deepin.system.SystemInfo", "/com/deepin/system/SystemInfo", "com.deepin.system.SystemInfo", QDBusConnection::systemBus())
                           .property("ProductName")
                           .toString();
     return  systemName;
