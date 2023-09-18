ebbb95a77ede0cc72409c4a23e3a8aeca9d9576f
houchengqiu <houchengqiu@uniontech.com>
2023-09-18 11:17:15 +0800

    fix: Application data directory requires permission to create tmp directory
       Application data directory requires permission to create tmp directory
    
    Log: Application data directory requires permission to create tmp directory

--- application/logallexportthread.cpp
+++ application/logallexportthread.cpp
@@ -182,7 +182,7 @@ void LogAllExportThread::run()
         QProcess procss;
         procss.setWorkingDirectory(tmpPath);
         QStringList arg = {"-c"};
-        arg.append(QString("zip -r tmp.zip ./*;mv tmp.zip '%1';chmod 666 '%2';chmod -R 666 '%3'").arg(m_outfile).arg(m_outfile).arg(Utils::getAppDataPath()));
+        arg.append(QString("zip -r tmp.zip ./*;mv tmp.zip '%1';chmod 777 '%2';chmod -R 777 '%3'").arg(m_outfile).arg(m_outfile).arg(Utils::getAppDataPath()));
         procss.start("/bin/bash", arg);
         procss.waitForFinished(-1);
         currentProcess += 9;
