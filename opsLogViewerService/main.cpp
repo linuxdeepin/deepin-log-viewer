e904b059e5a81d2b9f75b23d36071ee35810897c
gongheng <gongheng@uniontech.com>
2025-10-28 20:14:53 +0800

    Feat: [ops-log] Add ops log logic code for ops log.
    -- Add ops log logic code for ops log.
    
    Log: add feature for ops logs
    Task: https://pms.uniontech.com/story-view-39399.html

---
+++ opsLogViewerService/main.cpp
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
